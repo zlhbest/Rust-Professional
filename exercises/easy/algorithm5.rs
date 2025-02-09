bb6cc531c2045a7718c46bb61e90d8d22014a427
zhulonghao <zhulonghaolx@163.com>
2025-02-09 16:43:13 +0800

    完成bfs广度遍历

--- exercises/easy/algorithm5.rs
+++ exercises/easy/algorithm5.rs
@@ -1,40 +1,52 @@
 /*
-	bfs
-	This problem requires you to implement a basic BFS algorithm
+    bfs
+    BFS 广度优先算法
+    This problem requires you to implement a basic BFS algorithm
 */
-
 
 use std::collections::VecDeque;
 
 // Define a graph
 struct Graph {
-    adj: Vec<Vec<usize>>, 
+    adj: Vec<Vec<usize>>,
 }
 
 impl Graph {
     // Create a new graph with n vertices
     fn new(n: usize) -> Self {
         Graph {
+            // 这是一个邻接矩阵
             adj: vec![vec![]; n],
         }
     }
 
     // Add an edge to the graph
     fn add_edge(&mut self, src: usize, dest: usize) {
-        self.adj[src].push(dest); 
-        self.adj[dest].push(src); 
+        self.adj[src].push(dest);
+        self.adj[dest].push(src);
     }
 
     // Perform a breadth-first search on the graph, return the order of visited nodes
     fn bfs_with_return(&self, start: usize) -> Vec<usize> {
-        
-		//TODO
-
         let mut visit_order = vec![];
+        visit_order.push(start);
+        Self::bfs(&mut visit_order, &self, start);
         visit_order
     }
+    fn bfs(visit_order: &mut Vec<usize>, grapg: &Graph, start: usize) {
+        let mut vec = Vec::new();
+        for edge in &grapg.adj[start] {
+            if !visit_order.contains(edge) {
+                vec.push(edge.clone());
+            }
+        }
+        visit_order.extend(vec.clone());
+        // 然后依次bfs
+        for edge in vec {
+            Self::bfs(visit_order, grapg, edge);
+        }
+    }
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -84,4 +96,3 @@ mod tests {
         assert_eq!(visited_order, vec![0]);
     }
 }
-
--- exercises/easy/mod.rs
+++ exercises/easy/mod.rs
@@ -3,3 +3,4 @@ mod algorithm2;
 // 尝试使用其他排序完成
 mod algorithm3;
 mod algorithm4;
+mod algorithm5;
