1b621dcb54bcf62c8f944aa98c6178c73bb1574f
zhulonghao <zhulonghaolx@163.com>
2025-02-11 09:08:54 +0800

    完成第14题

--- exercises/easy/algorithm14.rs
+++ exercises/easy/algorithm14.rs
@@ -1,19 +1,41 @@
 /*
     Find Duplicates in Array
-    Given an array, find all the duplicate elements and return them. 
+    Given an array, find all the duplicate elements and return them.
     You need to solve the problem with O(1) space complexity (i.e., without using extra arrays or hash tables).
 
     Implement the function `find_duplicates(nums: Vec<i32>) -> Vec<i32>`.
     The function should return a vector containing all the duplicate elements in the array.
-    
+
     Hint: You can modify the input array in place to track duplicates.
 */
-
 use std::fmt::{self, Display, Formatter};
-
+/// o1的空间复杂度，不用额外的数组或者使用hashtable
 pub fn find_duplicates(nums: Vec<i32>) -> Vec<i32> {
-    // TODO: Implement the logic to find all duplicates in the array
-    Vec::new() // Placeholder return value
+    let mut nums = nums;
+    for i in 0..nums.len() {
+        let stay_value = nums[i];
+        if stay_value != 0 {
+            nums[i] = 0;
+            let mut is_repeat = false;
+            for j in i..nums.len() {
+                if nums[j] == stay_value && j != i {
+                    nums[j] = 0;
+                    is_repeat = true;
+                }
+            }
+            if is_repeat {
+                nums[i] = stay_value;
+            }
+        }
+    }
+    let mut nums = nums
+        .iter()
+        .filter(|item| **item != 0)
+        .map(|item| *item)
+        .collect::<Vec<i32>>();
+    nums.sort();
+    nums
+    // Placeholder return value
 }
 
 #[cfg(test)]
--- exercises/easy/mod.rs
+++ exercises/easy/mod.rs
@@ -3,6 +3,7 @@ mod algorithm10;
 mod algorithm11;
 mod algorithm12;
 mod algorithm13;
+mod algorithm14;
 mod algorithm2;
 /// 尝试使用其他排序完成
 mod algorithm3;
