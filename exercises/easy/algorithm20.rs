82c7c5079260f235a392b0365ec82dedcd674527
zhulonghao <zhulonghaolx@163.com>
2025-02-12 23:19:04 +0800

    第20题不使用加号使用异或与

--- exercises/easy/algorithm20.rs
+++ exercises/easy/algorithm20.rs
@@ -8,10 +8,22 @@
 */
 
 use std::fmt::{self, Display, Formatter};
-
+/// 在不使用加号的时候
+/// a与b的异或是处理的不同的值
+/// a与b的与处理的相同的值，左移就是进位了
+/// 在相加如果还有进位就再重复
 pub fn get_sum(a: i32, b: i32) -> i32 {
     // TODO: Implement the logic to calculate the sum of two integers without using `+`
-    a + b // Placeholder return value
+    // 异或是相同 = 0 不同 = 1 这就计算了当0和1的时候是1
+    let mut sum = a ^ b;
+    // a与b 算出来都是1的时候，如果 1 1 那结果就是1 将这个1左移，就代表进位
+    let mut con = (a & b) << 1;
+    while con != 0 {
+        let temp = sum & con;
+        sum = sum ^ con;
+        con = temp << 1;
+    }
+    sum
 }
 
 #[cfg(test)]
