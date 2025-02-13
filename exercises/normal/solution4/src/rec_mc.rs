0b01aaa9695ce3a0fe0ecaa4c62abcf2b22be256
zhulonghao <zhulonghaolx@163.com>
2025-02-13 11:13:24 +0800

    解决normal 第四题

--- exercises/normal/solution4/src/rec_mc.rs
+++ exercises/normal/solution4/src/rec_mc.rs
@@ -1,4 +1,20 @@
+const CASH_VALUE: &[u32] = &[1, 2, 5, 10, 20, 30, 50, 100];
 pub fn dp_rec_mc(amount: u32) -> u32 {
-    // TODO: 这里写逻辑
-    todo!()
+    // 这里明显就是使用贪心来处理
+    let mut money_value = amount;
+    let mut count = 0;
+    while money_value > 0 {
+        // 找需要的钱
+        let mut max_money = 0;
+        for i in CASH_VALUE {
+            if *i <= money_value {
+                if *i > max_money {
+                    max_money = *i;
+                }
+            }
+        }
+        count = count + 1;
+        money_value = money_value - max_money;
+    }
+    count
 }
