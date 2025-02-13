bc9ec854cd9b67eabcc06ef06b77adda870ce80a
zhulonghao <zhulonghaolx@163.com>
2025-02-13 10:48:25 +0800

    更新normal第一题

--- exercises/normal/solution1/src/count_distinct.rs
+++ exercises/normal/solution1/src/count_distinct.rs
@@ -1,3 +1,16 @@
 pub fn new_count_distinct(input_str: &str) -> usize {
-    todo!()
+    let mut bit_map = vec![false; 300usize];
+    let mut result = 0;
+    for item in input_str.split(",") {
+        let sum = item.chars().map(|char_item| char_item as usize).sum();
+        let hash_value = hash(sum, bit_map.len());
+        if !bit_map[hash_value] {
+            result += 1;
+            bit_map[hash_value] = true;
+        }
+    }
+    result
+}
+fn hash(char_number: usize, max_length: usize) -> usize {
+    char_number % max_length
 }
