7b197fb6993e39bd074af65333b1cd9046c718ac
zhulonghao <zhulonghaolx@163.com>
2025-02-13 11:07:21 +0800

    解决normal 第三题

--- exercises/normal/solution3/src/calc_logic.rs
+++ exercises/normal/solution3/src/calc_logic.rs
@@ -1,4 +1,9 @@
 pub fn new_birthday_probability(n: u32) -> f64 {
-    // TODO: 这里写逻辑
-    todo!()
+    // 实现算法的思路就是按照365天，看相反的数  存在两个及以上，那么就可以求都没有的概率
+    let years = 365;
+    let mut p = 1.0_f64;
+    for i in 1..=n {
+        p = p * ((years - i + 1) as f64 / years as f64);
+    }
+    1.0_f64 - p
 }
